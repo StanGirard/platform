1c800a9870f4a546e63c8bf409ab6fadfa67ad73
Stan Girard <girard.stanislas@gmail.com>
2023-12-12 23:15:43 +0100

    feat: Refactor ListFiles component to include download links

--- components/ListFiles.tsx
+++ components/ListFiles.tsx
@@ -13,48 +13,55 @@ type ListFilesProps = {
 };
 
 const ListFiles = (props: ListFilesProps): JSX.Element => {
-        const { files } = props;
+    const { files } = props;
 
-        return (
-                <div className="px-4 sm:px-6 lg:px-8">
-                    <div className="mt-8 flow-root">
-                        <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                            <div className="inline-block min-w-full py-2 align-middle">
-                                <table className="min-w-full divide-y divide-gray-300">
-                                    <thead>
-                                        <tr>
-                                            <th
-                                                scope="col"
-                                                className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6 lg:pl-8"
-                                            >
-                                                Name
-                                            </th>
-                                            <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                                                Size
-                                            </th>
-                                            <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                                                Similarity
-                                            </th>
-                                        </tr>
-                                    </thead>
-                                    <tbody className="divide-y divide-gray-200 bg-white">
-                                        {files.map((file) => (
-                                            <tr key={file.file_sha1}>
-                                                <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 lg:pl-8">
-                                                    {file.file_name}
-                                                </td>
-                                                <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{file.file_size}</td>
-                                                <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{file.file_similarity}</td>
-                                            </tr>
-                                        ))}
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
+    return (
+        <div className="px-4 sm:px-6 lg:px-8">
+            <div className="mt-8 flow-root">
+                <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div className="inline-block min-w-full py-2 align-middle">
+                        <table className="min-w-full divide-y divide-gray-300">
+                            <thead>
+                                <tr>
+                                    <th
+                                        scope="col"
+                                        className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6 lg:pl-8"
+                                    >
+                                        Name
+                                    </th>
+                                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                        Size
+                                    </th>
+                                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                        Similarity
+                                    </th>
+                                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                        Actions
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody className="divide-y divide-gray-200 bg-white">
+                                {files.map((file) => (
+                                    <tr key={file.file_sha1}>
+                                        <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 lg:pl-8">
+                                            {file.file_name}
+                                        </td>
+                                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{file.file_size}</td>
+                                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{file.file_similarity}</td>
+                                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                                            <a href={file.file_url} download>
+                                                Download
+                                            </a>
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
                     </div>
                 </div>
-        );
-}
-
+            </div>
+        </div>
+    );
+};
 
 export default ListFiles;
